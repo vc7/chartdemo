46d027e5084f2e298a786072e6110748f8fbe4b9
Vincent <vince78718@gmail.com>
2013-07-23 17:26:46 +0800

    setup the UI for iOS 7

--- chartdemo/chartdemo/Classes/CDRootView.m
+++ chartdemo/chartdemo/Classes/CDRootView.m
@@ -28,7 +28,21 @@
     if (self) {
         // Initialization code
         //
-        _titleLable = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, 120)];
+        
+        if ([IOS_VERSION doubleValue]>=7.0) {
+            _titleLable = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, frame.size.width, 120)];
+            _subtitleLable = [[UILabel alloc] initWithFrame:CGRectMake(0, 120, frame.size.width, 50)];
+            _chartView = [[CDPopulationChartView alloc] initWithFrame:CGRectMake(9, 85, frame.size.width, 150)];
+            _panelView = [[CDPanelView alloc] initWithFrame:CGRectMake(0, self.frame.size.height-80, self.frame.size.width, 80)];
+        }
+        else
+        {
+            _titleLable = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, 120)];
+            _subtitleLable = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, frame.size.width, 50)];
+            _chartView = [[CDPopulationChartView alloc] initWithFrame:CGRectMake(9, 75, frame.size.width, 150)];
+            _panelView = [[CDPanelView alloc] initWithFrame:CGRectMake(0, self.frame.size.height-100, self.frame.size.width, 80)];
+        }
+        
         [_titleLable setText:@"WORLD\nPOPULATION"];
         [_titleLable setTextColor:[UIColor whiteColor]];
         [_titleLable setTextAlignment:NSTextAlignmentCenter];
@@ -36,16 +50,13 @@
         [_titleLable setNumberOfLines:2];
         [_titleLable setBackgroundColor:[UIColor clearColor]];
         
-        _subtitleLable = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, frame.size.width, 50)];
         [_subtitleLable setText:@"Top ten most populous countries (millions)"];
         [_subtitleLable setTextColor:[UIColor whiteColor]];
         [_subtitleLable setTextAlignment:NSTextAlignmentCenter];
         [_subtitleLable setFont:[UIFont fontWithName:@"ProximaNova-Bold" size:12]];
         [_subtitleLable setBackgroundColor:[UIColor clearColor]];
         
-        _chartView = [[CDPopulationChartView alloc] initWithFrame:CGRectMake(9, 75, frame.size.width, 150)];
         
-        _panelView = [[CDPanelView alloc] initWithFrame:CGRectMake(0, self.frame.size.height-100, self.frame.size.width, 80)];
         _panelView.delegate = self;
         
         [self addSubview:_titleLable];
